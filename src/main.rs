7dc33a826796986ddc24e4db899e63655ceb795f
Alex Olteanu <alexandruolteanu42@gmail.com>
2024-07-04 21:29:55 +0100

    Respond with 200

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
-use std::net::TcpListener;
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").expect("Should be able to bind");
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(_) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {e}");
-            }
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(stream: std::io::Result<TcpStream>) {
+    match stream {
+        Ok(mut stream) => {
+            stream
+                .write_all(b"HTTP/1.1 200 OK\r\n\r\n")
+                .expect("All bytes should be written to the TCP stream");
+        }
+        Err(e) => {
+            println!("error: {e}");
         }
     }
 }
